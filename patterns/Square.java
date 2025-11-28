8298e2330e356340a5e9efbc6ebed9e5513026ac
shaik-sabeel <techsabeel1919@gmail.com>
2025-11-28 23:18:07 +0530

    Arrays p-1

---
+++ patterns/Square.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Square{
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 1; j<= 5-i+1; j++){
+                System.out.print("* ");
+            
+                
+            }
+            System.out.println("\n");
+        }
+        
+    }
+}
+ 
