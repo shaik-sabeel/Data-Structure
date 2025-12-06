3e8f54f961cad4c65f6dd1f67f8c98d1152b4ccd
shaik-sabeel <techsabeel1919@gmail.com>
2025-12-06 11:40:08 +0530

    2D-Arrays

--- practice.java
+++ practice.java
@@ -2,51 +2,17 @@ import java.util.*;
 
 public class practice {
 
-    public static void check(int num1[], int num2[]) {
-        
-        Arrays.sort(num1); // ensure sorted
-        Arrays.sort(num2); // ensure sorted
-
-        int m = num1.length - 1;
-        int n = num2.length - 1;
-        int k = num1.length + num2.length - 1;
-
-        int arr[] = new int[k + 1];
-
-        while (m >= 0 && n >= 0) {
-            if (num1[m] > num2[n]) {
-                arr[k] = num1[m];
-                m--;
-            } else {
-                arr[k] = num2[n];
-                n--;
+    public static void check(int num[]) {
+        int curr = 0;
+        while(curr < 0 || curr >= num.length )
+            if(num[curr] != 0){
+                curr++;
             }
-            k--;
-        }
-
-        while (n >= 0) {
-            arr[k] = num2[n];
-            n--;
-            k--;
-        }
-
-        while (m >= 0) {
-            arr[k] = num1[m];
-            m--;
-            k--;
-        }
-
-        int l = arr.length;
-
-        if(l % 2 == 0){
-            
-        }
     }
 
     public static void main(String[] args) {
-        int arr[] = {2, 1, 6};
-        int arr1[] = {3, 5, 2};
-
-        check(arr, arr1);
+        int arr[] = {1,0,2,0,3};
+        check(arr);
+        
     }
 }
