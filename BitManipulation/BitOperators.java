53ebfe619d534f3fab4a04ff197065990510e851
shaik-sabeel <techsabeel1919@gmail.com>
2025-12-22 05:57:38 +0530

    Bit Manipulation Started

---
+++ BitManipulation/BitOperators.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class BitOperators {
+
+    public static void main(String args[]) {
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~5);
+        System.out.println(6 >> 1);
+        System.out.println(5 << 2);
+        
+    }
+}
--- practice.java
+++ practice.java
@@ -1,29 +1,18 @@
 
-import java.util.*;
-
 public class Practice {
 
-    public static boolean arrayStringsAreEqual(String[] str1, String[] str2) {
-        String ch1 = "";
-        for (int i = 0; i < str1.length; i++) {
-            ch1 = ch1 + str1[i];
-        }
-        ch1 = ch1.toLowerCase();
-
-        String ch2 = "";
-        for (int i = 0; i < str2.length; i++) {
-            ch2 = ch2 + str2[i];
-        }
-        ch2 = ch2.toLowerCase();
-
-        return ch1.equals(ch2);
-    }
-
     public static void main(String[] args) {
+        int n = 5;
 
-        String[] str1 = {"abc", "d", "defg"};
-        String[] str2 = {"abc", "efg"};
-
-        System.out.println(arrayStringsAreEqual(str1, str2));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("");
+                }
+            }
+            System.out.println();
+        }
     }
 }
